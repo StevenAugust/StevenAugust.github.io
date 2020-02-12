9a6d948379874c706dac5fd28b1d4169e995c1d1
StevenAugust <slaugust945@gmail.com>
2020-02-12 20:31:20 +0000

    added gitignore file

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,24 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
-        // TODO 2 : Create a function that draws a circle 
+        var circle;
+        var circles = [];
         
-
+        
+        // TODO 2 : Create a function that draws a circle 
+        function drawCircle(){
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
+        
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+        var loopsCompleted = 0; 
+        while (loopsCompleted <= 100) {
+        drawCircle();
+        loopsCompleted++;
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,13 +49,18 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+            
             
             // TODO 5 : Call game.checkCirclePosition() on your circles.
+            
+            
            
-
             // TODO 8 : Iterate over the array
-           
+           for (var i = 0; i < circles.length; i++) {
+           var eachValue = circles[i];
+           physikz.updatePosition(circles[i]);
+           game.checkCirclePosition(circles[i]);
+           }
             
         }
     
@@ -55,18 +70,26 @@ var init = function (window) {
         it to the opposite side of the screen.
         */
         game.checkCirclePosition = function(circle) {
-
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if ( circle.x > canvas.width){
                 circle.x = 0;
             }
             
             // TODO 5 : YOUR CODE STARTS HERE //////////////////////
+            if( circle.y > canvas.height ){
+                circle.y = 0;
+            }
             
-
-
+            if( circle.x < 0){
+                circle.x = canvas.width;
+            }
+            
+            if( circle.y < 0){
+                circle.y = canvas.height;
+            }
+            
             // YOUR TODO 5 CODE ENDS HERE //////////////////////////
-        }
+        };
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
@@ -81,9 +104,8 @@ var init = function (window) {
         game.update = update;
         
         app.addUpdateable(window.opspark.game);
-    }
+    };
 };
-
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
